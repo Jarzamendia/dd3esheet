2c652830af1ab5960f9c64676d5a15c5278fffaa
Joao E. Arzamendia <joao.arzamendia@vsoft.com.br>
2024-05-20 21:19:28 -0300

    Adicionando filtros para monstros e magias

--- dd3esheet/sdr/urls.py
+++ dd3esheet/sdr/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("sdr/spells", views.spells, name="spells"),
     path("sdr/spell/<str:pk>", views.spell, name="spell"),
+    path("sdr/monsters", views.monsters, name="monsters"),
+    path("sdr/monster/<str:pk>", views.monster, name="monster"),
+
 ]
